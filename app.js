932d7ffed7d140137e816105218cdf13acdaf055
AshirAzeem-Developer <ashir9112001@gmail.com>
2022-11-01 23:39:20 +0500

    JS ASSIGNMENT 1 COMPLETE

---
+++ app.js
@@ -0,0 +1,143 @@
+//Chapter 1(Alerts)
+// alert("This Is JavaScrpt Assignment")
+
+// var name="Ashir Azeem"
+// window.alert(name);
+
+//-------------->>>>>Chapter 2 (Variable For Strings)<<<<<--------------
+
+// var NameOfAssignment;
+//  NameOfAssignment="JavaScript Assignment 1"
+//  alert(NameOfAssignment)
+
+//  NameOfAssignment="It Will Overwrite The Content ";
+//  alert(NameOfAssignment)
+
+// var lessonAuthor = "Mark";
+// var guyWhoKeepsSayingHisOwnName = "Mark";
+// var x = "Mark";
+// alert(lessonAuthor)
+// alert(guyWhoKeepsSayingHisOwnName)
+// alert(x)
+
+//-------------->>>>>Chapter 3 (Variable For Numbers)<<<<<--------------
+
+// var weight=150;
+
+// console.log(weight+25);
+
+// var originalNum = 23;
+// var newNum = originalNum + 7;
+
+// console.log(newNum);
+
+// var newNum2=newNum+originalNum+weight;
+// console.log(newNum2);
+
+// originalNum=originalNum+4;
+// console.log(originalNum);
+
+// originalNum=originalNum+"7";
+// console.log(originalNum);
+
+// originalNum=originalNum-"7";
+// console.log(originalNum);
+
+// var ConvertedTOString=144;
+
+// alert(ConvertedTOString);
+
+
+
+//-------------->>>>>Chapter 4 (Variable Name Legal And Illegal)<<<<<--------------
+
+//Legal And Illegeal Variables
+// var 1stNotLegal="Not Legal"
+// var Legal2Variable="Legal"
+// var Legal2Variable2="Legal"
+// var Legal2-Variable="Not Legal"
+// var Legal2_Variable="Legal"
+// var LegalVariable_="Legal"
+
+
+//-------------->>>>>Chapter 5 (Math Expression Familiar Operator)<<<<<--------------
+
+// var var1 = 4;
+// var var2 = 5 + 5;
+// var var2 = "5+5 Not Calculated With In the Quotes Taking It as String";
+// // alert(var2);
+// console.log(var2);
+
+// var popularNumber = 12 - 24;
+
+
+// var popularNumber = 3 * 12;
+
+// var num = 10;
+// var popularNumber = num + 200;
+
+// console.log(popularNumber);
+
+// var num = 10;
+// var anotherNum = 1;
+// var popularNumber = num + anotherNum;
+// console.log(popularNumber);
+
+// var whatsLeftOver = 10 % 3;
+// console.log(whatsLeftOver+" Is The Remaider");
+
+
+//-------------->>>>>Chapter 6 (Math Expression UnFamiliar Operator)<<<<<--------------
+
+// var num=1;
+// num++;
+// num=num+1;
+// console.log(num);
+
+// var num2=50;
+// num2--;
+// console.log(num2);
+
+// var newNum=num++;
+// console.log(newNum);
+// var newNum=+num;
+// console.log(newNum);
+// var newNum=num--;
+// console.log(newNum);
+// var newNum=--num;
+// console.log(newNum);
+
+//-------------->>>>>Chapter 7(Math Expression :Eliminating  Ambiguity)<<<<<--------------
+
+// var totalCost = 1 + 3 * 4;
+// console.log(totalCost+" Multiplied Then Added According to BODMAS");
+// var totalCost = 1 + (3 * 4);
+// console.log(totalCost+" Multiplied Then Added According to BODMAS");
+// var totalCost = (1 + 3) * 4;
+// console.log(totalCost+" Added Then Multiplied According to BODMAS");
+
+
+// var resultOfComputation = (2 * 4) * 4 + 2;
+// console.log(resultOfComputation+" BODMAS Applied Here");
+
+
+// resultOfComputation = ((2 * 4) * 4) + 2;
+// console.log(resultOfComputation+" BODMAS Applied Here");
+
+
+// resultOfComputation = (2 * 4) * (4 + 2);
+// console.log(resultOfComputation+" BODMAS Applied Here");
+
+//-------------->>>>>Chapter 8(Concatenating text strings  Ambiguity)<<<<<--------------
+
+// var message = "Chapter 8";
+// alert(message);
+
+// userName=" Ashir"
+// alert("Thanks, " + userName + "!");
+
+// var message = "Thanks, ";
+// var banger = "!";
+// alert(message + userName + banger);
+
+// alert("Chapter 8 Completed");
